ecaece72e458b185f1f7eb64abc362ef75549c53
Dennie Fuzi <fuzideni@gmail.com>
2023-05-30 19:50:53 +0700

    initial commit

---
+++ app/src/main/java/com/example/utspraktik_if10_10120918_denniefuzialviyanies/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.utspraktik_if10_10120918_denniefuzialviyanies;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
